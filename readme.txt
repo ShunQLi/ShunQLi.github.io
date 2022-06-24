b03233662b5223309852600d2bbe13a5b2f11237
ShunQ Li <li.shunq@northeastern.edu>
2022-06-24 13:26:54 -0400

    Page up

---
+++ readme.txt
@@ -0,0 +1,20 @@
+Info 6150
+Assignment 5A
+
+Shunqing Li
+
+Created a professional profolio with below Bootstrap components:
+
+Navbar
+Navs and tabs
+Badge
+Button
+Card
+Carousel
+Tooltip
+progressbar
+Forms
+Accordion
+Container
+
+Git page link: https://shunqli.github.io/
---
+++ script.js
@@ -0,0 +1,38 @@
+/* For progress bar animation*/
+(function ($) {
+    "use strict";
+
+    // Skills
+    $('.skill').waypoint(function () {
+        $('.progress .progress-bar').each(function () {
+            $(this).css("width", $(this).attr("aria-valuenow") + '%');
+        });
+    }, {offset: '80%'});
+
+})(jQuery);
+
+/* add class navbarDark on navbar scroll */
+const header = document.querySelector('.navbar');
+console.log(header)
+window.onscroll = function() {
+    const top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+
+
+/* collapse navbar after click on small devices */
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
+})
+
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
+  });
